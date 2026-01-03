655db0277adb5735c92347607d45e4a159047d43
Willian-Thrd <business.will-theo@outlook.com>
2026-01-03 00:05:31 -0300

    Atualizei umas coisas

--- src/main/java/org/will/MainController.java
+++ src/main/java/org/will/MainController.java
@@ -2,18 +2,25 @@ package org.will;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.*;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
 public class MainController {
-    @FXML
-    private ListView<File> lista;
+    ObservableList<Files> content = FXCollections.observableArrayList();
+    ListView<Files> lista;
+    
+    Path caminho = Paths.get("HQ_Files");
 
     @FXML
     private void initialize() throws IOException {
         System.out.println("Iniciando...");
+        content.add(Files.exists(caminho));
+        addArchive( null, content);
     }
 
     @FXML
@@ -28,5 +35,14 @@ public class MainController {
 
     // Elementos não FXML
 
+    private void addArchive(Files file, ObservableList<Files> list) {
+        ObservableList<Files> archives = FXCollections.observableArrayList();
+        
+        for (Files contents : list) {
+            archives.add(contents);
+        }
+
+        lista.setItems(archives);
+    } 
     
 }
